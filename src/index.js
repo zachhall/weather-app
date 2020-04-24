da4c1a72eb0c7d9a33f5f3c371ed1da17b2a1d70
Zach Hall <zachhallnc@gmail.com>
2020-04-24 10:21:02 -0400

    QUick change for Heroku deploy

--- src/index.js
+++ src/index.js
@@ -19,3 +19,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Quick change to deploye to Heroku
